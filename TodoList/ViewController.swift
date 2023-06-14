46d9c4c837db0d581599434349be3b1ff3ea2e50
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2023-06-14 18:04:15 +0900

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by AnnKangHo on 2023/06/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
