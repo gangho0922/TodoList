85b88b1bfbbb17c7ec96ae580cf9775e5017e761
Ann KangHo <102593699+gangho0922@users.noreply.github.com>
2023-06-17 10:06:01 +0900

    투두리스트 CRUD부분 alert생성 및 추가 완료

--- TodoList/ViewController.swift
+++ TodoList/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import CoreData
+import SnapKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -21,15 +22,49 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     private var models = [ListContents]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        getAllItems()
         title = "My To Do"
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
         tableView.frame = view.bounds
-        
-        //navigationItem.rightBarButtonItem = UIbar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
     }
     
+    @objc private func didTapAdd() {
+        let alert = UIAlertController(title: "새로 입력", message: "넣을 것을 입력해주세요.", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: "확인", style: .cancel, handler: { [weak self] _ in
+            guard let field = alert.textFields?.first, let text = field.text, !text.isEmpty else {
+                return
+            }
+            self?.createItem(name: text)
+        }))
+        present(alert, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let item = models[indexPath.row]
+        let sheet = UIAlertController(title: "수정", message: "수정할 것을 입력해주세요.", preferredStyle: .actionSheet)
+        sheet.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
+        sheet.addAction(UIAlertAction(title: "수정", style: .default, handler: { _ in
+            let alert = UIAlertController(title: "수정", message: "수정할 것을 입력해주세요.", preferredStyle: .alert)
+            alert.addTextField(configurationHandler: nil)
+            alert.textFields?.first?.text = item.sentence
+            alert.addAction(UIAlertAction(title: "저장", style: .cancel, handler: { [weak self] _ in
+                guard let field = alert.textFields?.first, let newSentence = field.text, !newSentence.isEmpty else {
+                    return
+                }
+                self?.updateItem(item: item, newSentence: newSentence)
+            }))
+            self.present(alert, animated: true)
+        }))
+        sheet.addAction(UIAlertAction(title: "삭제", style: .destructive, handler: { [weak self] _ in
+            self?.deleteItem(item: item)
+        }))
+        present(sheet, animated: true)
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
@@ -40,6 +75,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = model.sentence
         return cell
     }
+
     func getAllItems() {
         do {
             models = try context.fetch(ListContents.fetchRequest())
@@ -57,7 +93,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         newItem.sentence = name
         do {
             try context.save()
-            
+            getAllItems()
         } catch {
             
         }
@@ -66,7 +102,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         context.delete(item)
         do {
             try context.save()
-            
+            getAllItems()
         } catch {
             
         }
@@ -75,7 +111,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         item.sentence = newSentence
         do {
             try context.save()
-            
+            getAllItems()
         } catch {
             
         }
